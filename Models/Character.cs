cf2f16cb99d651f6b43b88273d09e06d434d179f
Abbos Engineer <abbosshamsiddin@gmail.com>
2022-12-15 07:09:23 +0500

    9-lesson done

---
+++ Models/Character.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace patrick.Models
+{
+    public class Character
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = "Abbos";
+        public int HitPoints { get; set; } = 100;
+        public int Strength { get; set; } = 10;
+        public int Defence { get; set; } = 10;
+        public int Intelligence { get; set; } = 10;
+        public RpgClass Class { get; set; } = RpgClass.Knight;
+    }
+}
---
+++ Models/RpgClass.cs
@@ -0,0 +1,9 @@
+namespace patrick.Models
+{
+    public enum RpgClass
+    {
+        Knight = 1,
+        Mage = 2,
+        Cleric = 3
+    }
+}
